fda252711949afd077f242337a8c624ab141c373
h <g>
2021-11-16 11:59:01 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp12
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double c,a=1,b=10;
+            Console.Write("Введите число:");
+            c = Convert.ToDouble(Console.ReadLine());
+            double i;
+            for (i=1;i<=b;i++)
+            {
+                Console.WriteLine($"{c}*{i}=" + (c * i));
+            }
+            Console.ReadKey();
+        }
+    }
+}
